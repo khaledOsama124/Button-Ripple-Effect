2811dc0a6490904feecaedc7d76be66d6aace6a9
khaledOsama <ko968880@gmail.com>
2022-11-21 18:24:44 +0200

    we use set properity method with this method we effect the button in two directions: 1-use can effect it at the horizan direction 2-use can effect it at the varient direction

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const btnEl=document.querySelector(".btn");
-btnEl.addEventListener("mouseover", (event)=>{
-    const x=(event.pageX - btnEl.offsetleft);
-    const y=(event.pageY - btnEl.offsettop );
+btnEl.addEventListener("mouseover", (event) => {
+    const x=event.pageX - btnEl.offsetleft;
+    const y=event.pageY - btnEl.offsettop ;
+
+    btnEl.style.setproperity("--XPos", x + "px");
+    btnEl.style.setproperity("--YPos", y + "px");
 
 
-}
+})
