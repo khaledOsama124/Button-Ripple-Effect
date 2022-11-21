84041ffac9ea601f00d01895ad90bce024233cb6
khaledOsama <ko968880@gmail.com>
2022-11-21 18:04:21 +0200

    when we open the console log when we go inside this button we can see a number inside the consle when we move it we can see different numbers we can effect button at horizen direction or varient direction

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const btnEl=document.querySelector(".btn");
 btnEl.addEventListener("mouseover", (event)=>{
-    
+    const x=(event.pageX - btnEl.offsetleft);
+    const y=(event.pageY - btnEl.offsettop );
+
 
 }
