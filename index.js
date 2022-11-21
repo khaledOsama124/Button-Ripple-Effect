c819026d067cc5db96c6b8f2df901e989da5e7f2
khaledOsama <ko968880@gmail.com>
2022-11-21 18:36:19 +0200

    call the variables and link it's value to condintes

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ btnEl.addEventListener("mouseover", (event) => {
     const x=event.pageX - btnEl.offsetleft;
     const y=event.pageY - btnEl.offsettop ;
 
-    btnEl.style.setproperity("--XPos", x + "px");
-    btnEl.style.setproperity("--YPos", y + "px");
+    btnEl.style.setproperity("--xPos", x + "px");
+    btnEl.style.setproperity("--yPos", y + "px");
 
 
 })
